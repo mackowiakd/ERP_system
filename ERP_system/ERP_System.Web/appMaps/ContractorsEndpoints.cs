6c4ff19268bf553f5ef05c5fc1410e92864f796d
FranciszekKrumschmidt <krumschmidt.franciszek@gmail.com>
2026-05-01 21:31:54 +0200

    admin panel button in contractors fixed

--- ERP_system/ERP_System.Web/appMaps/ContractorsEndpoints.cs
+++ ERP_system/ERP_System.Web/appMaps/ContractorsEndpoints.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.EntityFrameworkCore;
+using System.Text;
 
 namespace ERP_System.Web.appMaps
 {
@@ -12,18 +13,30 @@ namespace ERP_System.Web.appMaps
         public static void MapContractorEndpoints(this IEndpointRouteBuilder app)
         {
             // WYSWIETLANIE STRONY HTML
-            app.MapGet("/contractors", async context =>
+            app.MapGet("/contractors", async (HttpContext context, IWebHostEnvironment env, AppDbContext db) =>
             {
                 // Sprawdzamy ciasteczko user_id (zgodnie z LoginEndpoint)
-                if (!context.Request.Cookies.ContainsKey("user_id"))
-                {
-                    context.Response.Redirect("/");
-                    return;
-                }
+                if (!context.Request.Cookies.ContainsKey("logged_user"))
+                    return Results.Redirect("/");
 
-                context.Response.ContentType = "text/html; charset=utf-8";
-                await context.Response.SendFileAsync("wwwroot/contractors.html");
-            });
+                var userId = int.Parse(context.Request.Cookies["user_id"]!);
+                var user = await db.Employees.FirstOrDefaultAsync(u => u.Id == userId);
+                var username = context.Request.Cookies["logged_user"];
+                // context.Response.ContentType = "text/html; charset=utf-8";
+                var filePath = Path.Combine(env.WebRootPath, "contractors.html");
+                var html = File.ReadAllText(filePath, Encoding.UTF8);
+
+                html = html.Replace("{username}", username);
+                string adminBtnHtml = "";
+
+                if (user.Role == SystemRole.SystemAdmin)
+                {
+                    adminBtnHtml = "<button class=\"sidebar-link\" onclick=\"window.location.href='/adminConsole'\"><i class=\"fas fa-fw fa-cogs\"></i> &nbsp; Ustawienia Admina</button>";
+                }
+                html = html.Replace("{admin_panel_button}", adminBtnHtml);
+                return Results.Content(html, "text/html; charset=utf-8");
+                // await context.Response.SendFileAsync("wwwroot/contractors.html");
+            });                  
             
             // POBIERANIE LISTY
             app.MapGet("/api/contractors", async (HttpContext context, ContractorService service, AppDbContext db) =>
